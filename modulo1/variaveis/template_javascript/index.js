4c1a6d97422a41ac487a5f28406760a19ce691aa
Veronica Silveira <veronicasgc30@gmail.com>
2022-02-16 23:44:22 -0300

    exercicios variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,49 @@
+
+let a = 10
+let b = 10
+
+console.log(10)
+
+b = 5
+console.log(10, 5)
+
+2-
+
+
+console.log(10, 20, c) //undefined pois c não tem valor atribuído
+
+3-
+
+let horasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+let valorRecebidoDia = prompt("Quanto você recebe por dia?")
+alert(`Voce recebe ${horasTrabalhadasDia/valorRecebidoDia} por hora`)
+
+
+ let nome
+ let idade
+
+ console.log(typeof nome, typeof idade)
+
+ //porque nenhum valor foi atribuído as variáveis
+
+ const nome = prompt("Qual seu nome?")
+
+ const idade = prompt("Qual sua idade?")
+
+ const nome = "Veronica"
+ const idade = 31
+ console.log("Olá!" nome, "você tem" idade, "anos.")
+
+const comida = prompt("Você comeu sushi hoje?")
+const roupa = prompt("Você está usando blusa preta?")
+const atividadeFisica = prompt("Você foi na academia hoje?")
+
+const comida = "não"
+const roupa = "sim"
+const atividadeFisica = "sim"
+
+let a = 10
+let b = 25
+a = c
+b = a
+c = b
