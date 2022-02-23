cc82d2ac96728dc31dcac181c311c17c44021126
Verônica <veronicasgc30@gmil.com>
2022-02-22 22:25:22 -0300

    exercicio semana 2 objetos

---
+++ semana2/objetos/index.js
@@ -0,0 +1,85 @@
+//Exercícios de Interpretação de Código
+//Exercício 1
+//a) O console vai imprimir 
+//"Matheus Nachtergaele",
+// "12"
+// canal: "Globo", horario: "14h"
+
+//Exercício 2
+//a) O console vai imprimir 
+//nome Juca,	idade: 3, 	raca: SRD
+//"Juba"
+//Jubo
+
+//b) os três pontos antes de um objeto, copia as informações desse objeto para a declaração atual.
+
+//Exercício 3
+//a) false
+//undefined 
+//b)pois nao foi atruibuído valor a variável "altura"
+
+//Exercícios de escrita de código
+//Exercício 1
+
+//const objeto = {
+ //   nome: "Veronica",
+  //  apelidos: ["vevê", "flor de maio", "vê"]
+//}
+//function minhaFuncao(receber) {
+ //   return `Eu sou ${objeto.nome}, mas pode me chamar de ${objeto.apelidos[0]}, ${objeto.apelidos[1]} "ou" ${objeto.apelidos[2]}`
+//}
+
+//const novoObjeto = {
+ //   ...objeto,
+ //   novosApelidos: ["veh", "nikka", "verô"]
+//}
+//console.log(minhaFuncao(objeto))
+
+
+//Exercício 2
+//const formulario = {
+//    nome: "Veronica",
+//    idade: 31,
+//    profissao: "Educadora Física"
+// }
+
+// const formulario2 = {
+//    nome: "Pamela",
+//    idade: 32,
+//    profissao: "Telemarketing"
+// }
+
+// function imprimeFuncao(info) {
+//    return [
+// info.nome,
+// info.nome.length,
+// info.idade,
+// info.profissao,
+// info.profissao.length
+// ]
+// }
+// console.log(imprimeFuncao(formulario))
+// console.log(imprimeFuncao(formulario2)) 
+
+//Exercício 3
+//const carrinho = []
+//const fruta1 = {
+  //  nome:"laranja",
+ //   disponibilidade: true
+//} 
+//const fruta2 = {
+   // nome:"mamão",
+//disponibilidade: true
+//} 
+//const fruta3 = {
+ //   nome:"banana",
+ //   disponibilidade: true
+//} 
+//function noCarrinho(sacolao){
+  //  carrinho.push(sacolao)
+//}
+//noCarrinho(fruta1)
+//noCarrinho(fruta2)
+//noCarrinho(fruta3)
+//console.log(carrinho)
+
