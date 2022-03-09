cf0b07f07a4d580837bfbe2f901f336a5f384e5f
Verônica <veronicasgc30@gmil.com>
2022-03-08 23:18:26 -0300

    Exercicios Lacos incompleto

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,41 @@
+// Interpretação de código
+
+//Exercício 1
+
+// O código está gerando uma repetição sendo que o número deve ser menor
+// que 5. Ou seja, o console irá imprimir "0,1,2,3,4"
+
+//Exercício 2
+
+//a) O console vai imprimir os números menores que 18. Ou seja,
+//"10, 11, 12, 15"
+
+//b)
+
+//Exercício 3
+
+//Escrita de código
+
+//Exercício 1
+
+//a)
+
+let bichinhosDeEstimação = prompt("Quantos bichinhos de estimação você tem?")
+ {
+
+}
+//b)
+
+//c)
+
+//Exercício 2
+
+//a)
+
+//b)
+
+//c)
+
+//d)
+
+//e)
