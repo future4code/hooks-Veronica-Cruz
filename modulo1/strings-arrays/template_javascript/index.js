5712ec86599b1ea28e8fd4fa7e9647b6fc5d1b8d
Verônica <veronicasgc30@gmil.com>
2022-04-26 19:09:00 -0300

    alterações exercicios anteriores

--- modulo1/strings-arrays/template_javascript/index.js
+++ modulo1/strings-arrays/template_javascript/index.js
@@ -1,8 +1,11 @@
 // exercícios de interpretação de código
+
 //exercicio 1 - "a" array vazio ; "b" resulta em null ; "c" length: 10 ; "d,e,f" undefined 
+
 //exercicio 2 - "SUBI NUM ONIBUS EM MIRROCOS" 27
 
 //exercícios de escrita de código
+
 //exercicio 1
 
 //const nomeDoUsuario = prompt("Digite seu nome")
@@ -11,31 +14,64 @@
 
 //exercicio 2
 
+//A)
+
 const comidasPreferidas = ["sushi","lasanha","espaguete","maionese","frango assado"]
-console.log(comidasPreferidas)
-console.log("Essas são minhas comidas preferidas", comidasPreferidas[0])
-console.log(comidasPreferidas[1])
-console.log(comidasPreferidas[2])
-console.log(comidasPreferidas[3])
-console.log(comidasPreferidas[4])
+// console.log(comidasPreferidas)
 
-const comidaUsuario = prompt("Qual sua comida preferida?")
-const novaComida = comidasPreferidas.replaceAll("lasanha", comidaUsuario.push)
+//B)
 
+// console.log("Essas são minhas comidas preferidas:")
+// console.log(comidasPreferidas[0])
+// console.log(comidasPreferidas[1])
+// console.log(comidasPreferidas[2])
+// console.log(comidasPreferidas[3])
+// console.log(comidasPreferidas[4])
 
+//C)
+// comidasPreferidas[1] = prompt("Qual sua comida preferida?")
+
+// console.log(comidasPreferidas);
 
 
 //exercicio 3
 
-const listaDeTarefas = []
-//const tarefa1 = prompt("Você vai ao dentista?")
-listaDeTarefas.push(tarefa1)
-//const tarefa2 = prompt("Você vai a escola?")
-listaDeTarefas.push(tarefa2)
-//const tarefa3 = prompt("Você vai ao banco?")
-listaDeTarefas.push(tarefa3)
+//A)
+// const listaDeTarefas = []
 
-console.log(listaDeTarefas)
+//B)
+// const tarefa1 = prompt("Você vai ao dentista?")
+// listaDeTarefas.push(tarefa1)
+// const tarefa2 = prompt("Você vai a escola?")
+// listaDeTarefas.push(tarefa2)
+// const tarefa3 = prompt("Você vai ao banco?")
+// listaDeTarefas.push(tarefa3)
 
-//const tarefaRealizada = prompt("Digite o número da tarefa que realizou")
-listaDeTarefas.splice(2,1)
+
+//C)
+// console.log(['Você vai ao dentista?', listaDeTarefas[0]])
+// console.log(['Você vai a escola?', listaDeTarefas[1]])
+// console.log(['Você vai ao banco?' , listaDeTarefas[2]])
+
+//D)
+// const tarefaRealizada = prompt("Digite o número da tarefa que realizou")
+
+// E)
+// listaDeTarefas.splice(2,1)
+
+//F)
+
+// console.log(listaDeTarefas)
+//------------------------------------------------------------------------------
+
+//DESAFIOS
+
+//1- 
+
+const frase = "Meu nome é Verônica"
+
+const fraseNoArray = [frase.trim]
+
+
+
+console.log(fraseNoArray)
