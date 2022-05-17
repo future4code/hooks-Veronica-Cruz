f6270a779eaaeddeeb49cb1aa918d4a174116779
Verônica <veronicasgc30@gmil.com>
2022-05-17 19:05:48 -0300

    exercicio https e postman

---
+++ modulo3/rend-condicional-form/renderizacao-condicional/src/components/Etapa1.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styledComponents from 'styled-components';
+
+
+
+
+function PrimeiraEtapa() {
+    return (
+      <div className="App">
+        <h2>Qual seu nome?</h2>
+        <input/>
+
+        <h2>Quantos anos você tem?</h2>
+        <input/>
+
+        <h2>Digite seu e-mail:</h2>
+        <input/>
+
+        <h2>Qual o seu grau de escolaridade?</h2>
+
+<ul>
+    <li>Ensino Médio Incompleto</li>
+    <li>Ensino Médio Completo</li>
+    <li>Ensino Superior Incompleto</li>
+    <li>Ensino Superior Completo</li>
+
+</ul>
+      </div>
+    );
+  }
+  
+  export default PrimeiraEtapa;
+
+
