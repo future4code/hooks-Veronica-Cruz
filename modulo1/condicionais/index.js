d2ea931939a410316162a7a251a0dd2796124895
Verônica <veronicasgc30@gmil.com>
2022-06-14 14:49:26 -0300

    exercicios condicionais atualizados

--- modulo1/condicionais/index.js
+++ modulo1/condicionais/index.js
@@ -24,7 +24,9 @@
 //a) o código está pedindo para o usuário digitar um número
 
 //b) se o usuário digitar 10, o console vai imprimir "Esse número passou no teste"
-// se o usuário digitar -10, o console vai dar erro.
+// se o usuário digitar -10, o console vai dar erro, pois não tem condicional caso
+//o número seja menor que zero.
+
 
 //c) haverá erro no console sim, pois o  "if" é uma condicional e precisaria ter o 
 // "else" para se caso o número não fosse maior que zero.
@@ -33,14 +35,14 @@
 
 //Exercício 1
 
-/* const idadeDoUsuario = Number(prompt("Escreva aqui qual é a sua idade?"))
+// const idadeDoUsuario = Number(prompt("Escreva aqui qual é a sua idade?"))
 
-if (idadeDoUsuario >18) {
-    console.log("Você pode dirigir")
-} 
-else (idadeDoUsuario <18)  {
-    console.log("Você não pode dirigir")
-} */
+// if (idadeDoUsuario >18) {
+//     console.log("Você pode dirigir")
+// } 
+// else if (idadeDoUsuario < 18)  {
+//     console.log("Você não pode dirigir")
+// }
 
 //Exercício 2
 
@@ -56,34 +58,119 @@ if(turnoAluno.toUpperCase() === "M") {
 
 //Exercício 3
 
-/* const turnoAluno= prompt("Qual o turno do seu curso? Digite M(Manhã), V(Verspertino/Tarde) ou N(Noturno)")
+// const turnoAluno= prompt("Qual o turno do seu curso? Digite M(Manhã), V(Verspertino/Tarde) ou N(Noturno)")
 
-switch (turnoAluno.toUpperCase()) {
-case 'M':
-console.log("Bom dia")
-break
-case 'V':
-console.log("Boa Tarde")
-break
-case 'N':
-console.log("Boa Noite")
-} */
+// switch (turnoAluno.toUpperCase()) {
+// case 'M':
+// console.log("Bom dia")
+// break
+// case 'V':
+// console.log("Boa Tarde")
+// break
+// case 'N':
+// console.log("Boa Noite")
+// }
 
 //Exercício 4
 
-const generoFilme = prompt("Qual o gênero do filme?")
-const ingresso = Number(prompt("Qual o valor do ingresso?"))
-const amigo = prompt("Vai assistir ao filme com um amigo? Sim ou Não")
-const genero = "fantasia"
-const valorIngresso = 15
+// const generoFilme = prompt("Qual o gênero do filme?")
+// const ingresso = Number(prompt("Qual o valor do ingresso?"))
 
+// if (generoFilme === "fantasia" && ingresso < 15) {
+//    console.log ("Bom Filme!")
+// } else {
+//     console.log ("Escolha outro filme :(")
+// }
 
-if (generoFilme === "fantasia") {
+//DESAFIOS:
 
-} else if(valorIngresso < 15) {
+//exercício 1
 
-} else if (amigo.toUpperCase === "Sim"){
-    console.log("Bom filme")
-} else if (amigo.toUpperCase === "Não"){
-    console.log("Escolha outro filme :(")
+// const generoFilme = prompt("Qual o gênero do filme?")
+// const ingresso = Number(prompt("Qual o valor do ingresso?"))
+// const lanchinho = prompt("Qual lanchinho você vai querer para acompanhar seu filme?")
+
+// if (generoFilme === "fantasia" && ingresso < 15) {
+//    console.log ("Bom Filme!", "Aproveite seu", lanchinho)
+// } else {
+//     console.log ("Escolha outro filme :(")
+// }
+
+//exercicio 2
+
+const infoUsuario = (valor) => {
+    //pegar informações do usuário
+ const nome = prompt("Digite seu nome completo").toUpperCase()
+ const tipoDeJogo = prompt("Qual será o tipo de jogo? Digite IN para Jogo Internacional ou DO para Jogo Doméstico").toUpperCase()
+ const etapaDoJogo = prompt("Em qual etapa? Digite SF para Semi-Final, DT para Decisão de Terceiro Lugar ou FI para Final").toUpperCase()
+ const categoria = Number(prompt("Qual a categoria? Digite as opções 1, 2, 3 ou 4"))
+ const quantidadeDeIngressos = Number(prompt("Qual a quantidade de ingressos?"))
+
+console.log('Nome do Cliente:',nome)
+console.log('Internacional ou Doméstico:', tipoDeJogo)
+console.log('Etapa Do Jogo:',etapaDoJogo)
+console.log('Categoria:',categoria)
+console.log('Quantidade de Ingressos:',quantidadeDeIngressos)
+
+    //tabela de preços
+ const tabelaDePrecosSF = {
+    SFCat1 : 1350,
+    SFCat2 : 880,
+    SFCat3 : 550,
+    SFCat4 : 220
+ }
+
+ const tabelaDePrecosDT = {
+    DTCat1 : 660,
+    DTCat2 : 440,
+    DTCat3 : 330,
+    DTCat4 : 170
+ }
+
+ const tabelaDePrecosFI = {
+    FICat1 : 1980,
+    FICat2 : 1320,
+    FICat3 : 880,
+    FICat4 : 330
+ }
+
+ 
+    //condicional para imprimir no console as informações do usuário e o valor total 
+
+    const condicionalTotal = () => {
+        if (etapaDoJogo === 'SF'.toUpperCase() && categoria === 1) {
+            console.log('Valor do Ingresso:',tabelaDePrecosSF.SFCat1,'Total:',tabelaDePrecosSF.SFCat1 * quantidadeDeIngressos)
+         } else if (etapaDoJogo === 'SF'.toUpperCase() && categoria === 2) {
+            console.log('Valor do Ingresso:',tabelaDePrecosSF.SFCat2,'Total:',tabelaDePrecosSF.SFCat2 * quantidadeDeIngressos)
+         }else if (etapaDoJogo === 'SF'.toUpperCase() && categoria === 3) {
+            console.log('Valor do Ingresso:',tabelaDePrecosSF.SFCat3,'Total:',tabelaDePrecosSF.SFCat3 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'SF'.toUpperCase() && categoria === 4) {
+            console.log('Valor do Ingresso:',tabelaDePrecosSF.SFCat4,'Total:',tabelaDePrecosSF.SFCat4 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'DT'.toUpperCase() && categoria === 1 ) {
+            console.log('Valor do Ingresso:',tabelaDePrecosDT.DTCat1,'Total:',tabelaDePrecosDT.DTCat1 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'DT'.toUpperCase() && categoria === 2) {
+            console.log('Valor do Ingresso:',tabelaDePrecosDT.DTCat2,'Total:',tabelaDePrecosDT.DTCat2 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'DT'.toUpperCase() && categoria === 3) {
+            console.log('Valor do Ingresso:',tabelaDePrecosDT.DTCat3,'Total:',tabelaDePrecosDT.DTCat3 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'DT'.toUpperCase() && categoria === 4) {
+            console.log('Valor do Ingresso:',tabelaDePrecosDT.DTCat4,'Total:',tabelaDePrecosDT.DTCat4 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'FI'.toUpperCase() && categoria === 1) {
+            console.log('Valor do Ingresso:',tabelaDePrecosFI.FICat1,'Total:',tabelaDePrecosFI.FICat1 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'FI'.toUpperCase() && categoria === 2) {
+            console.log('Valor do Ingresso:',tabelaDePrecosFI.FICat2,'Total:',tabelaDePrecosFI.FICat2 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'FI'.toUpperCase() && categoria === 3) {
+            console.log('Valor do Ingresso:',tabelaDePrecosFI.FICat3,'Total:',tabelaDePrecosFI.FICat3 * quantidadeDeIngressos)
+        }else if (etapaDoJogo === 'FI'.toUpperCase() && categoria === 4) {
+            console.log('Valor do Ingresso:',tabelaDePrecosFI.FICat4,'Total:',tabelaDePrecosFI.FICat4 * quantidadeDeIngressos)
+        }
+    } 
+    return condicionalTotal()
+
 }
+
+infoUsuario()
+ 
+
+
+
+
