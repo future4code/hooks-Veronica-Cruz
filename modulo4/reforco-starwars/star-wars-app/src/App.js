6d716aa499a9792e57f4fb0a620c0a1cbe47aea3
Verônica S <veronicasgc30@gmail.com>
2022-07-19 17:13:29 -0300

    projeto star wars

---
+++ modulo4/reforco-starwars/star-wars-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "./routes/Router";
+
+function App() {
+  return (
+    <div className="App">
+   
+      <h1>Star Wars</h1>
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
---
+++ modulo4/reforco-starwars/star-wars-app/src/routes/Router.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import CharacterDetailPage from "../pages/CharacterDetailPage/CharacterDetailPage";
+import CharacterListPage from "../pages/CharacterListPage/CharacterListPage";
+
+const Router = () => {
+    return(
+        <BrowserRouter>
+        <Routes>
+            <Route path="/detalhes" element={<CharacterDetailPage/>}/>
+            <Route path="/lista" element={<CharacterListPage/>}/>
+        </Routes>
+        </BrowserRouter>
+    )
+}
+export default Router;
