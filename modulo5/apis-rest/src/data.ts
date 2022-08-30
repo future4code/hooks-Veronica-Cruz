68c04c2d0ce24348199c4bc6a818a06671c80600
Verônica S <veronicasgc30@gmail.com>
2022-08-30 18:36:34 -0300

    atualizaçõs exercicio apis-rest

--- modulo5/apis-rest/src/data.ts
+++ modulo5/apis-rest/src/data.ts
@@ -10,47 +10,47 @@ export type User = {
     type: UserType,
     age: number
 }
-export let users = [
+export let users: User[] = [
     {
         id: 1,
         name: "Alice",
         email: "alice@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 12
     },
     {
         id: 2,
         name: "Bob",
         email: "bob@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 36
     },
     {
         id: 3,
         name: "Coragem",
         email: "coragem@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 21
     },
     {
         id: 4,
         name: "Dory",
         email: "dory@email.com",
-        type: "NORMAL",
+        type: UserType.NORMAL,
         age: 17
     },
     {
         id: 5,
         name: "Elsa",
         email: "elsa@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 17
     },
     {
         id: 6,
         name: "Fred",
         email: "fred@email.com",
-        type: "ADMIN",
+        type: UserType.ADMIN,
         age: 60
     }
 ]
