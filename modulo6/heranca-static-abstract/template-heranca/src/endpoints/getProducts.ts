fbcf2cc670d02b2ea61ab478303b99f0a16e8ea2
Verônica S <veronicasgc30@gmail.com>
2022-10-03 18:39:01 -0300

    atualização exericicio herança

--- modulo6/heranca-static-abstract/template-heranca/src/endpoints/getProducts.ts
+++ modulo6/heranca-static-abstract/template-heranca/src/endpoints/getProducts.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express"
-import connection from "../database/connection"
-import { TABLE_PRODUCTS } from "../database/tableNames"
+import { UserDatabase } from "../database/UserDatabase"
 
 export const getProducts = async (req: Request, res: Response) => {
     let errorCode = 400
     try {
-        const result = await connection(TABLE_PRODUCTS).select()
+        // const result = await connection(TABLE_PRODUCTS).select()
+        const userDatabase = new UserDatabase()
+        const result = await userDatabase.getAllProducts()
         res.status(200).send({ products: result })
+
     } catch (error) {
         res.status(errorCode).send({ message: error.message })
     }
--- modulo6/heranca-static-abstract/template-heranca/src/endpoints/getUsers.ts
+++ modulo6/heranca-static-abstract/template-heranca/src/endpoints/getUsers.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from "express"
-import connection from "../database/connection"
-import { TABLE_USERS } from "../database/tableNames"
+import { UserDatabase } from "../database/UserDatabase"
 
 export const getUsers = async (req: Request, res: Response) => {
     let errorCode = 400
     try {
-        const result = await connection(TABLE_USERS).select()
+        // const result = await connection(TABLE_USERS).select()
+        const userDatabase = new UserDatabase()
+        const result = await userDatabase.getAllUsers()
         res.status(200).send({ users: result })
     } catch (error) {
         res.status(errorCode).send({ message: error.message })
