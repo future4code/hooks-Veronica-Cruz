948c957996b5417c3af98bb2d1a55e3ee3e11ae8
Verônica S <veronicasgc30@gmail.com>
2023-02-12 21:03:36 -0300

    responsividade e design mensagem

--- modulo2/projeto-portifolio/script.js
+++ modulo2/projeto-portifolio/script.js
@@ -1,9 +1,20 @@
-function abrirMenu(){
-    let barraMenuAberto = document.getElementById('barra-menu')
-    if(barraMenuAberto.style.width === "0px"){
-        barraMenuAberto.style.width = "180px";
-    }else{
-        barraMenuAberto.style.width = "0px";
-    }
-
+function abrirMenu() {
+  let barraMenuAberto = document.getElementById("barra-menu");
+  if (barraMenuAberto.style.width === "0px") {
+    barraMenuAberto.style.width = "180px";
+  } else {
+    barraMenuAberto.style.width = "0px";
+  }
 }
+
+
+function botaoVoltar() {
+  window.location = "index.html";
+}
+
+
+function saberMais() {
+    document.getElementById("contato");
+    console.log(saberMais());
+}
+
